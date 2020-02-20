6e2b4e46229e855ddf326fbc4f0207bef2e7c163
Austin Riley <Austin.Riley@fivetalent.com>
2020-02-20 15:57:22 -0800

    Add starter code for storage

--- src/components/Media/Gallery.js
+++ src/components/Media/Gallery.js
@@ -1,10 +1,62 @@
 import React, { useState, useEffect } from 'react';
 
+const LEVEL = "protected";
+
 function PhotoGallery(lastRefreshTime) {
+
+  const [ photoList, setPhotoList ] = useState([]);
+
+  useEffect(() => {
+    getImages();
+  }, [lastRefreshTime])
+
+  const getImages = async () => {
+    
+    const images = [];
+    
+    //TODO: Get images from S3
+
+    setPhotoList(images);
+  }
+
+  const renderPhotoList = () => {
+    if(!photoList || photoList.length === 0) {
+      return "You have not yet added any images";
+    }
+
+    return photoList.map((item) => {
+      //TODO: Render images in list
+
+      return null;
+    });
+  }
+
+  const promptForRemoval = key => {
+    const confirmResult = window.confirm("Are you sure you want to delete this photo?");
+
+    if (confirmResult === true) {
+      removePhoto(key);
+    } 
+  }
+
+  const removePhoto = async (key) => {
+    try {
+      //TODO: Remove photo from S3
+
+      let newPhotoList = photoList.filter(function( obj ) {
+        return obj.key !== key;
+      });
+
+      setPhotoList(newPhotoList);
+    }
+    catch(error) {
+      console.error("Error Removing Photo: ", error);
+    }
+  }
 
   return (
     <div className="flex flex-wrap justify-center items-center container m-auto">
-      TODO: Implement
+      { renderPhotoList() }
     </div>
   );
 }
--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -1,12 +1,81 @@
 import React, { useState } from 'react';
-
 import PhotoGallery from './Gallery';
 
 function Media() {
+  const [selectedImage, setSelectedImage] = useState();
+  const [imagePreview, setImagePreview] = useState('');
+  const [lastUploadTime, setLastUploadTime] = useState('');
+
+  const uploadImage = async (level) => {
+    if(!selectedImage) {
+      alert("No Image Selected");
+      return;
+    }
+
+    const uploadResult = null; //TODO: Upload image to S3
+
+    if(uploadResult && uploadResult.key) {
+      clearImageSelection();
+      setLastUploadTime(new Date());
+    }
+  }
+
+  const clearImageSelection = () => {
+    setImagePreview('');
+    setSelectedImage('');
+  }
+
+  const renderImageSelection = () => {
+    return "TODO ADD IMAGE SELECTION USING AMPLIFY REACT COMPONENTS";
+  }
+
+  const renderImageUpload = () => {
+    return (
+      <React.Fragment>
+        <h3 className="text-2xl font-bold text-blue-700 m-2">Selected Image</h3>
+        <img src={imagePreview} className="w-1/2 m-auto" alt="upload preview" />
+
+        <div className="flex justify-center m-4">
+          <button 
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline curstor-pointer m-2" 
+            onClick={() => uploadImage('private')} 
+            type="button"
+          >
+            Upload Private Image
+          </button>
+          <button 
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline curstor-pointer m-2" 
+            onClick={() => uploadImage('protected')} 
+            type="button"
+          >
+            Upload Protected Image
+          </button>
+          <button 
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline curstor-pointer m-2" 
+            onClick={() => uploadImage('public')} 
+            type="button"
+          >
+            Upload Public Image
+          </button>
+          <button 
+            className="bg-white hover:bg-blue-500  hover:text-white text-blue-500 font-bold border-2 border-blue-500 py-2 px-4 rounded-full cursor-pointer m-2" 
+            onClick={() => clearImageSelection()} 
+            type="button"
+          >
+            Pick New Image
+          </button>
+        </div>
+      </React.Fragment>
+    );
+  }
 
   return (
-    <div className="flex flex-col justify-center items-center container m-auto">
-      TODO: Implement
+    <div className="flex flex-col justify-center items-center container m-auto pb-12">
+      {!selectedImage ? renderImageSelection() : renderImageUpload()}
+
+      <div className="flex flex-col w-1/2 m-auto mt-4">
+        TODO RENDER IMAGE GALLERY
+      </div> 
     </div>
   );
 }
